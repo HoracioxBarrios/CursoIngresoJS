e2ef4e6d44d482d6e841117555750785987a3d76
horacio barrios <horacioxbarrios@gmail.com>
2023-03-07 18:16:51 -0300

    Mejorado

--- 0-Atencion_Rescursos_Parcial/Atencio_Recursos_Codigo.js
+++ 0-Atencion_Rescursos_Parcial/Atencio_Recursos_Codigo.js
@@ -12,5 +12,31 @@ promedio = acumulador / contador;
 
 //******************************************************* */
 
+// porcentaje
+//formula 
+//porcentaje = (valor / total) * 100
+
+/**si en una zapateria entran 5 hombres, y en ese dia entraron 5 clientes
+  el porcentaje de ventas seria 100% */              
+let porcentaje;
+let contadorZapatos;
+let contadorGeneral;
+
+contadorZapatos = 0;
+contadorGeneral = 0
+
+//a manopla
+contadorZapatos = 5;
+contadorGeneral = 10;
+//               5 zapatos         //  10 contador clientes que ingresaron en total
+porcentaje = (contadorZapatos / contadorGeneral)* 100;
+
+//porcentaje de ventas = 50%
+
+//******************************************************* */
+
+
+
+
 
 
--- 0-Primer_Parcial_Hecho/Ejercicio2.js
+++ 0-Primer_Parcial_Hecho/Ejercicio2.js
@@ -1,4 +1,4 @@
-/*
+b/*
 Ejercicio 2:
 
 Apellido: Barrios
@@ -18,3 +18,138 @@ A) Horario promedio de concurrencia.
 B) El porcentaje de clientes por género
 C) Nombre y genero del cliente de menor edad que haya visto una pelicula de Heroes.
 D) El tipo de pelicula mas concurrida"*/
+
+function mostrar() {
+    let nombre;
+    let edad;
+    let genero;
+    let tipoPelicula;
+    let horario;
+    let respuesta;
+    let acumHorarios;
+    let contadorGeneralTickets;
+    let promedioHorarios;
+    let contadorMasculinos;
+    let contadorFemeninos;
+    let contadorNb;
+    let porcentajeHombres;
+    let porcentajeMujeres;
+    let porcentajeNb;
+    let contadorTerror;
+    let contadorHeroes;
+    let contadorRomance;
+    let tipoPeliculaMasConcurrida;
+    let nombreMinimo;
+    let generoMinimo;
+    let edadMinima;
+    let flagEdadMinima;
+
+    respuesta = true;
+    acumHorarios = 0;
+    contadorGeneralTickets = 0;
+    contadorMasculinos = 0;
+    contadorFemeninos = 0;
+    contadorNb = 0;
+    contadorTerror = 0;
+    contadorHeroes = 0;
+    contadorRomance = 0;
+    flagEdadMinima = true;
+
+
+    while (respuesta == true) {
+
+        nombre = prompt("Ingrese nombre");
+
+        do {
+            edad = prompt("ingrese edad");
+            edad = parseInt(edad);
+        } while (edad < 13);// 12 no me deja, tiene que ser mayor a 12
+
+        do {
+            genero = prompt("ingrese genero");
+        } while (genero != "F" && genero != "M" && genero != "NB");
+
+        do {
+            tipoPelicula = prompt("ingrese tipo de pelicula");
+        } while (tipoPelicula != "Terror" && tipoPelicula != "Heroes" && tipoPelicula != "Romance");
+
+        do {
+            horario = prompt("ingrese horario");
+            horario = parseInt(horario);
+        } while (horario < 8 || horario > 23);
+
+        //A) Horario promedio de concurrencia.
+        acumHorarios += horario; // acum = acum + hora;
+
+
+        //B) El porcentaje de clientes por género
+        switch (genero) {
+            case "F":
+                contadorFemeninos++;
+                break;
+            case "M":
+                contadorMasculinos++;
+                break;
+            case "NB":
+                contadorNb++;
+                break;
+        }
+
+        switch (tipoPelicula) {
+            case "Terror":
+                contadorTerror++;
+                break;
+            case "Heroes":
+                contadorHeroes++;
+                break;
+            case "Romance":
+                contadorRomance++;
+                break;
+        }
+
+        if(flagEdadMinima == true){
+            edadMinima = edad;
+            nombreMinimo = nombre;
+            generoMinimo = genero;
+        }else{
+            if(edad < edadMinima){
+                edadMinima = edad;
+                nombreMinimo = nombre;
+                generoMinimo = genero;
+            }
+        }
+
+        contadorGeneralTickets++;
+        respuesta = confirm("desea seguir ?");
+    }//while
+
+    promedioHorarios = acumHorarios / contadorGeneralTickets;
+
+    porcentajeMujeres = (contadorFemeninos / contadorGeneralTickets) * 100;
+    porcentajeHombres = (contadorMasculinos / contadorGeneralTickets) * 100;
+    porcentajeNb = (contadorNb / contadorGeneralTickets) * 100;
+
+    //D) El tipo de pelicula mas concurrida"
+    if (contadorTerror > contadorHeroes && contadorTerror > contadorRomance) {
+        tipoPeliculaMasConcurrida = "Terror";
+    } else {
+        if (contadorHeroes > contadorRomance) {
+            tipoPeliculaMasConcurrida = "Heroes";
+        } else {
+            tipoPeliculaMasConcurrida = "Romance";
+        }
+    }
+
+    //A
+    document.write("Promedio de horarios: " + promedioHorarios + "</br>" );
+
+    //B
+    document.write("Porcentaje por Generos: Mujeres " + porcentajeMujeres.toFixed(2) + " Hombres " + porcentajeHombres.toFixed(2) + " Nb " + porcentajeNb.toFixed(2) + "</br>");
+
+    //C
+    document.write("Nombre del cliente de menor edad que haya visto una pelicula de Heroes." + nombreMinimo + " genero " + generoMinimo + "</br>");
+
+    //D
+    document.write("El tipo de pelicula mas concurrida: " + tipoPeliculaMasConcurrida + "</br>");
+
+}//fin
